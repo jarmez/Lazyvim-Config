788d39f8e010ef8c73c9b43eb8b3469d478382a1
James Day <james@day.net.nz>
2025-10-03 21:15:57 +1300

    Updated nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,40 +1,14 @@
 return {
   -- Add the catppuccin plugin and configure it
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato", -- Set the theme flavor to Macchiato
-      })
-    end,
-  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Override LazyVim's default config for bufferline.nvim
-  {
-    "akinsho/bufferline.nvim",
-    lazy = true, -- Add this line
-    dependencies = { "catppuccin/nvim" },
-    init = function()
-      -- Temporarily patch the catppuccin integration
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      if not bufline.get then
-        bufline.get = bufline.get_theme
-      end
-    end,
-    opts = {
-      options = {
-        highlights = require("catppuccin.groups.integrations.bufferline").get_theme(),
-      },
-    },
-  },
 
   -- Configure LazyVim to load the catppuccin theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
